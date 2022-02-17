7f0fa8f62981fea72059932541aac06e509cb07c
Matt Holton <mtholton@valdosta.edu>
2022-02-17 16:24:37 +0000

    updated for deliverable

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import "./index.css";
 
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
+// import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import { Provider } from 'react-redux';
 
 import App from './components/App';
@@ -12,7 +11,7 @@ import rootReducer from './reducers';
 
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
-const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware()));
+const store = createStore(rootReducer, {}, applyMiddleware());
 
 ReactDOM.render(
     <Provider store={store}>
